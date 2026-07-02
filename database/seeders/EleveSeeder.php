1285d27f42a2a3b9f7f2738ee0caa90c64ce6438
wybounty <siberfrancis971@gmail.com>
2026-07-01 20:35:23 -0400

    Eleves pages

--- database/seeders/EleveSeeder.php
+++ database/seeders/EleveSeeder.php
@@ -18,42 +18,42 @@ class EleveSeeder extends Seeder
             'user_id' => 1,
             'nom' => 'Doe',
             'prenom' => 'John',
-            'photo' => 'https://via.placeholder.com/150',
+            'photo' => 'https://media.istockphoto.com/id/2151669184/vector/vector-flat-illustration-in-grayscale-avatar-user-profile-person-icon-gender-neutral.jpg?s=612x612&w=0&k=20&c=UEa7oHoOL30ynvmJzSCIPrwwopJdfqzBs0q69ezQoM8=',
         ]);
 
         Eleve::create([
             'user_id' => 1,
             'nom' => 'Doe',
             'prenom' => 'John',
-            'photo' => 'https://via.placeholder.com/150',
+            'photo' => 'https://media.istockphoto.com/id/2151669184/vector/vector-flat-illustration-in-grayscale-avatar-user-profile-person-icon-gender-neutral.jpg?s=612x612&w=0&k=20&c=UEa7oHoOL30ynvmJzSCIPrwwopJdfqzBs0q69ezQoM8=',
         ]);
 
         Eleve::create([
             'user_id' => 1,
             'nom' => 'Doe',
             'prenom' => 'John',
-            'photo' => 'https://via.placeholder.com/150',
+            'photo' => 'https://media.istockphoto.com/id/2151669184/vector/vector-flat-illustration-in-grayscale-avatar-user-profile-person-icon-gender-neutral.jpg?s=612x612&w=0&k=20&c=UEa7oHoOL30ynvmJzSCIPrwwopJdfqzBs0q69ezQoM8=',
         ]);
 
         Eleve::create([
             'user_id' => 1,
             'nom' => 'Doe',
             'prenom' => 'John',
-            'photo' => 'https://via.placeholder.com/150',
+            'photo' => 'https://media.istockphoto.com/id/2151669184/vector/vector-flat-illustration-in-grayscale-avatar-user-profile-person-icon-gender-neutral.jpg?s=612x612&w=0&k=20&c=UEa7oHoOL30ynvmJzSCIPrwwopJdfqzBs0q69ezQoM8=',
         ]);
 
         Eleve::create([
             'user_id' => 1,
             'nom' => 'Doe',
             'prenom' => 'John',
-            'photo' => 'https://via.placeholder.com/150',
+            'photo' => 'https://media.istockphoto.com/id/2151669184/vector/vector-flat-illustration-in-grayscale-avatar-user-profile-person-icon-gender-neutral.jpg?s=612x612&w=0&k=20&c=UEa7oHoOL30ynvmJzSCIPrwwopJdfqzBs0q69ezQoM8=',
         ]);
 
         Eleve::create([
             'user_id' => 1,
             'nom' => 'Doe',
             'prenom' => 'John',
-            'photo' => 'https://via.placeholder.com/150',
+            'photo' => 'https://media.istockphoto.com/id/2151669184/vector/vector-flat-illustration-in-grayscale-avatar-user-profile-person-icon-gender-neutral.jpg?s=612x612&w=0&k=20&c=UEa7oHoOL30ynvmJzSCIPrwwopJdfqzBs0q69ezQoM8=',
         ]);
     }
 }
